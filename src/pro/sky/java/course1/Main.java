f19cd55f5b27bd5d0a093a60941a095ef15b24da
AKolomeytsev <a.kolomeytsev@list.ru>
2021-10-12 21:59:10 +0300

    Домашнее задание. Условные опраторы.

--- src/pro/sky/java/course1/Main.java
+++ src/pro/sky/java/course1/Main.java
@@ -5,86 +5,130 @@ public class Main {
     public static void main(String[] args) {
         //work 1
 
-        byte a = 1;
-        short b = 32000;
-        int c = 34_000;
-        long d = 10_000_000_000_000L;
-
-        float e = 3.14f;
-        double f = 3.33333333333;
-
+        int clientOS = 1;
+        if(clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else
+            if(clientOS == 0){
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Работаем с сайтом");
+            }
         //work 2
 
-        float firstBoxer = 78.2f;
-        float secondBoxer = 82.7f;
-        float sumWeight = firstBoxer+secondBoxer;
-        float diffWeight = secondBoxer - firstBoxer;
-        System.out.println("Общий вес боксеров равен: " + sumWeight + " кг");
-        System.out.println("Разность в весе боксеров составляет: " + diffWeight + " кг");
+        int clientDeviceYear = 2015;
+        if(clientDeviceYear<2015){
+            if(clientOS == 1) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else if (clientOS == 0) {
+                        System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                    } else {
+                        System.out.println("Работаем с сайтом");
+                    }
+        } else if(clientOS == 1) {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                } else if(clientOS == 0){
+                            System.out.println("Установите версию приложения для iOS по ссылке");
+                        } else {
+                            System.out.println("Работаем с сайтом");
+                        }
 
         //work 3
-
-        int bananasWeight = 80;
-        int bananasCount = 5;
-        int milkWeight = 105;
-        int milkCount = 200;
-        int iceCreamWeight = 100;
-        int iceCreamCount = 2;
-        int eggWeight = 70;
-        int eggCount = 4;
-
-        float sumWeightMix = (bananasWeight * bananasCount) + (milkWeight * milkCount) + (iceCreamWeight * iceCreamCount) + (eggWeight * eggCount);
-        float sumWeightMixKilogram = sumWeightMix/1000;
-        System.out.println("Общий вес микса в граммах: " + sumWeightMix + " г");
-        System.out.println("Общий вес микса в килограммах: " + sumWeightMixKilogram + " кг");
+        int year = 1600;
+        boolean isLeapYear = (year%4==0 && year%100!=0) || year%400==0;
+        if(isLeapYear)
+            System.out.println(year + " год является високосным");
+        else
+            System.out.println(year + " год не является високосным");
 
         //work 4
 
-        int extraWeight = 7*1000;
-        int stepWeight1 = 250;
-        int stepWeight2 = 500;
-        int step1 = extraWeight/stepWeight1;
-        int step2 = extraWeight/stepWeight2;
-        int avgDay = extraWeight/((stepWeight1+stepWeight2)/2);
-        System.out.println("Потеря веса " + stepWeight1 + "г - " + step1 +  " дней");
-        System.out.println("Потеря веса " + stepWeight2 + "г - " + step2 + " дней");
-        System.out.println("Среднее количество дней:" + avgDay);
+        double deliveryDistance = 25;
+        int day = 0;
+        if(deliveryDistance<=20)
+            day = 1;
+        else if(deliveryDistance>20 && deliveryDistance<60)
+                day = 2;
+            else  if(deliveryDistance>=60 && deliveryDistance<=100)
+                    day = 3;
+        System.out.println("Потребуется дней: "+day);
 
         //work 5
+        int monthNumber = 3;
+        String season = "";
+        switch (monthNumber){
+            case 1:
+                season = "Зима";
+                break;
+            case 2:
+                season = "Зима";
+                break;
+            case 3:
+                season = "Весна";
+                break;
+            case 4:
+                season = "Весна";
+                break;
+            case 5:
+                season = "Весна";
+                break;
+            case 6:
+                season = "Лето";
+                break;
+            case 7:
+                season = "Лето";
+                break;
+            case 8:
+                season = "Лето";
+                break;
+            case 9:
+                season = "Осень";
+                break;
+            case 10:
+                season = "Осень";
+                break;
+            case 11:
+                season = "Осень";
+                break;
+            case 12:
+                season = "Зима";
+                break;
+            default:
+                season = "Месяц задан не правильно. Сезон не определен";
+        }
+        System.out.println(season);
 
-        double masha = 67760;
-        double denis = 83690;
-        double kristina = 76230;
-        double mashaNew = masha + masha*.1;
-        double denisNew = denis + denis*.1;
-        double kristinaNew = kristina + kristina*.1;
-        System.out.println("Маша теперь получает:" + mashaNew + " рублей. Годовой доход вырос на " + (mashaNew*12-masha*12) +  " рублей");
-        System.out.println("Денис теперь получает:" + denisNew + " рублей. Годовой доход вырос на " + (denisNew*12-denis*12) +  " рублей");
-        System.out.println("Кристина теперь получает:" + kristinaNew + " рублей. Годовой доход вырос на " + (kristinaNew*12-kristina*12) +  " рублей");
+        // work 6
+        int age = 36;
+        double salary = 5000;
+        double totalLimit = 0;
 
-        //work 6
+        if(salary>=50000 && salary<80000)
+            totalLimit = salary*1.2;
+        else if(salary>=80000)
+                totalLimit = salary*1.5;
+        else
+            totalLimit = salary;
 
-        int a1 = 12;
-        int b1 = 27;
-        int c1 = 44;
-        int d1 = 15;
-        int e1 = 9;
-        int result = (a1 * (b1 + (c1 - d1 * e1)));
-        System.out.println(result*-1);
+        if(age<=23)
+            totalLimit = totalLimit*2;
+        else
+            totalLimit = totalLimit*3;
+
+        System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + totalLimit + "рублей");
 
         // work 7
-        int a2 = 5;
-        int b2 = 7;
-        System.out.println("a = " + a2 + " b = " + b2);
-        a2 = a2+b2;
-        b2 = a2-b2;
-        a2 = a2-b2;
-        System.out.println("a = " + a2 + " b = " + b2);
-
-        // work 8
-        int x = 834;
-        int y = x/10%10;
-
-        System.out.println(y);
+        double baseRate = 10;
+        double wantedSum = 330_000;
+        if(age<23)
+            baseRate = baseRate + 1;
+        else if(age<30)
+            baseRate = baseRate + .5;
+        if(salary>80_000)
+            baseRate = baseRate - .7;
+        if(baseRate/100*wantedSum/12<=salary/2)
+            System.out.println("Максимальный платеж при ЗП " + salary + " равен " + salary/2 + " рублей. Платеж по кредиту " + baseRate/100*wantedSum/12 + "рублей. Одобрено.");
+        else
+            System.out.println("Максимальный платеж при ЗП " + salary + " равен " + salary/2 + " рублей. Платеж по кредиту " + baseRate/100*wantedSum/12 + "рублей. Отказано");
     }
 }
