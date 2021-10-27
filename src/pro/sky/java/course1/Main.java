b4ab4e72dd9339e374718c369b165b33c63ead3f
AKolomeytsev <a.kolomeytsev@list.ru>
2021-10-27 22:31:25 +0300

    Домашнее задание. Методы.

--- src/pro/sky/java/course1/Main.java
+++ src/pro/sky/java/course1/Main.java
@@ -1,85 +1,119 @@
 package pro.sky.java.course1;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) {
-        // work 1
-        String firstName = "Артём";
-        String middleName = "Сергеевич";
-        String lastName = "Коломейцев";
-        String fullName = lastName + " " + firstName + " " + middleName;
-        System.out.println("ФИО сотрудника — " + fullName);
-        System.out.println();
-
-        //work 2
-        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
-        System.out.println();
-
-        //work 3
-        System.out.println("Данные ФИО сотрудника для административного отдела — " + fullName.replace(" ","; "));
-        System.out.println();
-
-        //work 4
-        if(fullName.contains("ё"))
-            fullName = fullName.replace("ё","е");
-        System.out.println("Данные ФИО сотрудника — " + fullName);
-        System.out.println();
-
-        //work 5
-        fullName = fullName.trim();
-        boolean goodString = fullName.indexOf(" ")>-1 && fullName.lastIndexOf(" ")>-1 && fullName.indexOf(" ")!=fullName.lastIndexOf(" ");
-        if(goodString){
-            lastName =  fullName.substring(0,fullName.indexOf(" "));
-            firstName = fullName.substring(fullName.indexOf(" ")+1,fullName.lastIndexOf(" "));
-            middleName = fullName.substring(fullName.lastIndexOf(" ")+1);
-
-            System.out.println("Имя сотрудника — " + firstName);
-            System.out.println("Фамилия сотрудника — " + lastName);
-            System.out.println("Отчество сотрудника — " + middleName);
-
-        } else {
-            System.out.println("Строка задана не верно");
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
         }
-        System.out.println();
-
-        //work 6
-        fullName = lastName.toLowerCase() + " " + firstName.toLowerCase() + " " + middleName.toLowerCase();
-        goodString = fullName.indexOf(" ")>-1 && fullName.lastIndexOf(" ")>-1 && fullName.indexOf(" ")!=fullName.lastIndexOf(" ");
-        if(goodString){
-            char[] arrayFullName = fullName.toCharArray();
-            int indexName = fullName.indexOf(" ")+1;
-            int indexMiddleName = fullName.lastIndexOf(" ")+1;
-            String newfullName = String.valueOf(arrayFullName[0]).toUpperCase() + fullName.substring(1,fullName.indexOf(" "));
-            newfullName = newfullName + " "  + String.valueOf(arrayFullName[indexName]).toUpperCase() + fullName.substring(fullName.indexOf(" ")+2,fullName.lastIndexOf(" "));
-            newfullName = newfullName + " "  + String.valueOf(arrayFullName[indexMiddleName]).toUpperCase() + fullName.substring(fullName.lastIndexOf(" ")+2);
-            fullName = newfullName;
-            System.out.println("Верное написание Ф. И. О. сотрудника с заглавных букв —  " + fullName);
-        } else {
-            System.out.println("Строка задана не верно");
+        return arr;
+    }
+    public static String kindYear(int y){
+        boolean isLeapYear = (y%4==0 && y%100!=0) || y%400==0;
+        if(isLeapYear){
+            return "високосный год";
+        }else{
+            return "не високосный год";
         }
-        System.out.println();
-
-        //work 7
-        StringBuilder firstStr = new StringBuilder("135");
-        StringBuilder secondStr = new StringBuilder("246");
-        String newString = "";
-        for(int i=0;i<firstStr.length();i++){
-            newString = newString + String.valueOf(firstStr.charAt(i)) + String.valueOf(secondStr.charAt(i));
+    }
+    public static String kindVersionOs(int t,int y){
+        String os = "";
+        String version = "";
+        int currentYear = LocalDate.now().getYear();
+        if (y < currentYear) {
+            version = "lite";
+            if (t == 1) {
+                os = "Android";
+            } else if (t == 0) {
+                os = "iOS";
+            }
+        } else version = "обычную";
+        if (t == 1) {
+            os = "Android";
+        } else if (t == 0) {
+            os = "iOS";
         }
-        System.out.println(newString);
-
-        //work 8
-        String str = "aabccddefgghiijjkk";
-        String newStr = "";
-        for(int i=0;i<str.length();i++){
+        return "Установите " + version + " версию " + os;
+    }
+    public static int deliveryDay(int d){
+        int day = 0;
+        if (d <= 20)
+            day = 1;
+        else if (d > 20 && d < 60)
+            day = 2;
+        else if (d >= 60 && d <= 100)
+            day = 3;
+        return day;
+    }
+    public static void validText(String str){
+        String message = "Нет дублей";
+        for(int i=0;i<str.length()-1;i++){
             if(str.charAt(i) == str.charAt(i+1)){
-                newStr = newStr + String.valueOf(str.charAt(i));
-                i++;
+                message = "Дублирутся '" + str.charAt(i) + "' символы " + i + " и " + (i+1);
+                break;
             }
         }
-        System.out.println(newStr);
+        System.out.println(message);
+    }
+    public static void reversArr(int[] arr){
+        if(arr.length%2==0){
+            for(int i = 0;i<arr.length/2;i++){
+                change(arr, i, ((arr.length - 1) - i));
+            }
+        }else {
+            for (int i = 0; i != (arr.length - 1) - i; i++) {
+                change(arr, i, ((arr.length - 1) - i));
+            }
+        }
+    }
+    public static void change(int[] arr,int e1,int e2){
+        arr[e1] = arr[e1]+arr[e2];;
+        arr[e2] = arr[e1]-arr[e2];
+        arr[e1] = arr[e1]-arr[e2];
+    }
+    public static double argSalary(int[] arr){
+        double arg = 0;
+        int sum = sum(arr);
+        arg = sum/arr.length;
+        return arg;
+    }
+    public static int sum(int[] arr){
+        int sum = 0;
+        for(int i = 0;i<arr.length;i++){
+            sum += arr[i];
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        // work 1
+        int year = 2021;
+        System.out.println(year + "-" + kindYear(year));
 
+        // work 2
+        int version = 1;
+        int curYear = 2015;
+        System.out.println(kindVersionOs(version,curYear));
+
+        // work 3
+        int deliveryDistance = 25;
+        System.out.println("Потребуется дней: "+deliveryDay(deliveryDistance));
+
+        // work 4
+        String str = "aabccddefgghiijjkk";
+        validText(str);
+
+        // work 5
+        int[] arr = {3, 2, 1, 6, 5};
+        reversArr(arr);
+        System.out.println(Arrays.toString(arr));
+
+        // work 6
+        int[] arrSalary = generateRandomArray();
+        System.out.println(argSalary(arrSalary));
     }
 }
